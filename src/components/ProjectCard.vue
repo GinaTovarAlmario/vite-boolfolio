727dc54085102ca7cd3c477330eae27b720b4148
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-14 21:10:22 +0100

    created a new component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<template>
+    <div class="card mb-3" v-for="singleProject in projectList" :key="singleProject.id">
+        <div class="card-body">
+            <h5 class="card-title">
+                Title: {{ singleProject.title }}
+            </h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">
+                Author: {{ singleProject.author }}
+            </h6>
+            <p class="card-text">
+                Date: {{ singleProject.date }}
+            </p>
+            <p class="card-text">
+                Status: {{ singleProject.status }}
+            </p>
+            <p class="card-text">
+                Type: {{ singleProject.type.name }}
+            </p>
+            <p class="card-text">
+                Technologies: {{ singleProject.technologies.name }}
+            </p>
+            <p class="card-text">
+                Description: {{ singleProject.description }}
+            </p>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss"></style>
