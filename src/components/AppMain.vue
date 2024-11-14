f5edfab25bd4a052265d6d32d42c9c8294ac1c45
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-14 18:17:16 +0100

    added a basic structure for a card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,15 @@
 <script>
 import axios from 'axios';
 export default {
-    name:'AppMain',
-    data(){
-        return{
+    name: 'AppMain',
+    data() {
+        return {
 
         }
     },
-    methods:{
+    methods: {
         // metodo per recuperare i projects
-        getProjects(){
+        getProjects() {
 
         }
 
@@ -18,11 +18,29 @@ export default {
 </script>
 
 <template>
-    <h1>
-        APPMAIN HELLOOO
-    </h1>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mt-3">
+                    <h2 class="mt-3 text-center fw-bold">
+                        Projects List:
+                    </h2>
+                </div>
+                <div class="col-12">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
