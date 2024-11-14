a4f853c17e9e0519cb8d11a40b18b96fd77ea280
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-14 17:22:41 +0100

    added name and name method

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,19 @@
 <script>
 import axios from 'axios';
 export default {
+    name:'AppMain',
     data(){
         return{
 
         }
-    }
+    },
+    methods:{
+        // metodo per recuperare i projects
+        getProjects(){
+
+        }
+
+    },
 }
 </script>
 
