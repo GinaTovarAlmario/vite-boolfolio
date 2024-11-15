8b0ce71fc7c64664d48a326a95b8c02588f08adc
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-15 16:31:01 +0100

    handled the case I have no technolgies and type to be safe

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -32,11 +32,15 @@ export default {
                 Status: {{ cardItem.status }}
             </p>
             <p class="card-text">
-                Type: {{ cardItem.type.name }}
+                Type: {{ cardItem.type ? cardItem.type.name : 'None' }}
             </p>
             <p class="card-text">
-                <!-- vorrei che fossero scritte uno accanto all'altra -->
-                Technologies: {{ cardItem.technologies.map(tech => tech.name).join(', ') }}
+                <!-- vorrei che fossero scritte uno accanto all'altra e gestire il caso in cui 
+                venga modificata la validazione fatta nel backoffice e quindi gestire il caso in cui non ci sia -->
+                Technologies:
+                {{ cardItem.technologies && cardItem.technologies.length > 0
+                    ? cardItem.technologies.map(tech => tech.name).join(', ')
+                    : 'None' }}
             </p>
             <p class="card-text">
                 Description: {{ cardItem.description }}
