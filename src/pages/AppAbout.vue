c754930851268c2e83fa4243c19079b0d3dbfe31
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-15 17:37:34 +0100

    created appabout.vue

---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'AppAbout',
+    data() {
+        return {
+            projectList :[],
+        }
+    },
+    components:{
+        ProjectCard,
+    },
+    methods: {
+    }
+
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mt-3">
+                    <h2>
+                        About Us
+                    </h2>
+                </div>
+                <div class="col-12 text-center">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo perferendis, consequatur temporibus illum cumque harum, nisi modi accusantium provident commodi cupiditate fuga non est iste quae veniam fugiat minus similique?
+                        Ea itaque quas sint amet officia odio laboriosam, repudiandae maiores, quisquam impedit tenetur, deleniti rem. Culpa, architecto quas a aut dicta dolorum minima odit? Vero culpa assumenda ad nisi ea!
+                        Corrupti quis magni aliquid repellat ratione possimus perspiciatis fugiat, unde, deleniti quaerat sapiente assumenda nobis necessitatibus facilis maxime officia eligendi at. Rem consequatur id modi? Iure voluptatem provident ab ea!
+                        Iste quibusdam, omnis esse id veritatis illo cum odio distinctio hic inventore vel, iure quasi nisi voluptate architecto magni adipisci nesciunt laboriosam exercitationem sequi? Ratione voluptate doloribus expedita iste alias.
+                        Aliquam ipsam numquam ratione obcaecati quos itaque et temporibus accusamus similique modi neque nulla beatae nostrum dolor qui sapiente labore, sint odio voluptatum a? Obcaecati temporibus labore earum alias exercitationem!
+                        Officiis voluptate, doloribus accusamus error modi odit consequatur hic at sapiente, autem veritatis? Nam doloremque ab consectetur, eius cupiditate facilis exercitationem maiores enim. Harum voluptatum accusantium enim, repudiandae dolores sit.
+                        Architecto quae voluptas provident eligendi, porro quam hic similique commodi cupiditate corporis eveniet unde harum laborum consequatur, aperiam perspiciatis at dolore pariatur. Velit veniam minus, quam nihil temporibus cumque exercitationem.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
