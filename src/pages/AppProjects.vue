558207bed374847c843de54641631ce2ef46a71f
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-18 17:07:46 +0100

    created appproject and added componenets

---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,34 @@
+<script>
+import ProjectList from '../components/ProjectList.vue';
+
+export default {
+    name: 'AppProjects',
+    data() {
+        return {
+        }
+    },
+    components: {
+        ProjectList,
+    }
+
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mt-3">
+                    <h2>
+                        Project List:
+                    </h2>
+                    <ProjectList/>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
