d164b8ac97adf55c72c5c19d87c9c2da1be989e5
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-18 17:07:21 +0100

    modified projectlist

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,48 @@
+<script>
+import axios from 'axios';
+import ProjectListCard from './ProjectListCard.vue';
+export default {
+    name: 'ProjectList',
+    data() {
+        return {
+            projectList: [],
+        }
+    },
+    components: {
+        ProjectListCard,
+    },
+    methods: {
+        // metodo per recuperare i projects
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.projectList = response.data.results;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+    created() {
+        this.getProjects();
+
+    }
+}
+</script>
+
+<template>
+    <section class="col-12" id="project-list">
+        <ProjectListCard 
+        v-for="projectItem in projectList" 
+        :key="projectItem.id" 
+        :card="projectItem" />
+    </section>
+</template>
+
+<style scoped lang="scss">
+main {
+    background-color: blueviolet;
+
+}
+</style>
