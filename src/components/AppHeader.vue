dbae09f1f36e204330c26082f59bf7c80d653684
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-18 16:08:29 +0100

    created appheader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+
+}
+</script>
+
+<template>
+   <header>
+        <nav>
+            <ul>
+                <li>
+                    link
+                </li>
+                <li>
+                    link
+                </li>
+            </ul>
+        </nav>
+   </header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
