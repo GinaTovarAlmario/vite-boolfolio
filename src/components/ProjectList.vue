dc466eb0073d5a72de56f35331d3aa84f9a80363
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 17:40:25 +0100

    added apiUrl

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -18,7 +18,7 @@ export default {
     methods: {
         // metodo per recuperare i projects
         getProjects(pageNumber) {
-            axios.get({
+            axios.get(this.apiUrl, {
                 params: {
                     page:pageNumber,
                 }
