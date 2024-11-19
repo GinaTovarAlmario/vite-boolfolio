4add5d2d82d3ee660b87ab8e65581dc1182e9b60
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 22:36:43 +0100

    created apptypes

---
+++ src/pages/AppTypes.vue
@@ -0,0 +1,36 @@
+<script>
+import ProjectList from '../components/ProjectList.vue';
+
+export default {
+    name: 'AppProjects',
+    data() {
+        return {
+        }
+    },
+    components: {
+        ProjectList,
+    }
+
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mt-3 text-center">
+                    <h2>
+                        Types List:
+                    </h2>
+                </div>
+                <div class="col-8 m-auto">
+                    <ProjectList/>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
