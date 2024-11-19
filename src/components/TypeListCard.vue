235f5e2c4c0bbdfb3dab615c2a2ae8fd3e5b16c6
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 22:51:58 +0100

    fixed typelistcard structure

---
+++ src/components/TypeListCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name:'TypeListCard',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        typeObj: {
+            type: Object,
+            required: true
+        }
+    },
+
+}
+</script>
+<template>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title">
+                name: {{ typeObj.name }}
+            </h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">
+                id: {{ typeObj.id }}
+            </h6>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss"></style>
