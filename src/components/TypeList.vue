94cb2b4aa09e642fe1f180a26ee2538ed2ca100c
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 22:49:20 +0100

    fixed structure for typelist

---
+++ src/components/TypeList.vue
@@ -0,0 +1,60 @@
+<script>
+import axios from 'axios';
+import AppLoader from './AppLoader.vue';
+import TypeListCard from './TypeListCard.vue';
+export default {
+    name: 'TypeList',
+    data() {
+        return {
+            typesList: [],
+            loaded: false,
+            apiUrl: 'http://127.0.0.1:8000/api/types',
+        }
+    },
+    components: {
+        TypeListCard,
+        AppLoader
+    },
+    methods: {
+        // metodo per recuperare i projects
+        getTypes() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    console.log(response);
+                    this.typesList = response;
+                    this.loaded = true;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        },
+        showSingleType(typeId){
+            this.$router.push({ name:'types.show', params:{ id:typeId}})
+        }
+        
+    },
+    created() {
+        this.getTypes();
+
+    },
+}
+</script>
+
+<template>
+    <section class="loader mb-4 mt-4" v-if="!loaded">
+        <AppLoader />
+    </section>
+    <section class="col-12" id="project-list" v-else>
+        <TypeListCard v-for="type in typesList" 
+        :key="type.id" 
+        :typeObj="type" 
+        @click="showSingleProject(type.id)"/>
+    </section>
+    <section class="navigation">
+        
+    </section>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
