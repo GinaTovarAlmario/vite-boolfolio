9d95734379879987f9b812b398e9f90dd29ec42d
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 22:56:00 +0100

    added route api to types

--- src/js/router.js
+++ src/js/router.js
@@ -4,11 +4,13 @@ import AppHome from '../pages/AppHome.vue'
 import AppAbout from '../pages/AppAbout.vue'
 import AppProjects from '../pages/AppProjects.vue'
 import AppSingleProject from '../pages/AppSingleProject.vue'
+import AppTypes from '../pages/AppTypes.vue'
 
 const routes = [
   { path: '/', component: AppHome , name:'home' },
   { path: '/about', component: AppAbout, name:'about' },
   { path: '/projects', component: AppProjects, name:'projects' },
+  { path: '/types', component: AppTypes, name:'types' },
   { path: '/projects/:id', component: AppSingleProject, name:'projects.show' },
 
 
