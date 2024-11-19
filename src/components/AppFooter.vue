059f2c4df676b9fcf7847cd5adc42a073f1029b9
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 17:09:02 +0100

    added dimensions and color to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,34 +12,33 @@ export default {
 </script>
 
 <template>
-   <footer class="mt-4">
-        <nav>
-            <ul class="d-flex justify-content-center">
-                <li class="me-3">
-                    <router-link>
-                        link footer
-                    </router-link> 
+    <footer class="mt-4">
+        <nav class="bg-dark text-white">
+            <ul class="list-style-none py-2">
+                <li class="nav-item">
+                    <router-link :to="{ name: 'home' }" aria-current="page" class="nav-link">
+                        HomePage
+                    </router-link>
                 </li>
+                <li class="nav-item">
                 <li class="me-3">
-                    <router-link>
-                        link footer
-                    </router-link> 
+                    <router-link :to="{ name: 'about' }" aria-current="page" class="nav-link">
+                        Abou tUs
+                    </router-link>
                 </li>
-                <li class="me-3">
-                    <router-link>
-                        link footer
-                    </router-link> 
                 </li>
-                <li class="me-3">
-                    <router-link>
-                        link footer
-                    </router-link> 
+                <li class="nav-item">
+                    <router-link :to="{ name: 'projects' }" aria-current="page" class="nav-link">
+                        Latest Projects
+                    </router-link>
                 </li>
             </ul>
         </nav>
-   </footer>
+    </footer>
 </template>
 
 <style scoped lang="scss">
-
+nav{
+    height: 100px;
+}
 </style>
